dc87ffcdbf1a53f30a92cbf82866874dc3e81f02
kafi.62 <kafiraj.27@gmail.com>
2020-03-03 01:34:04 +0600

    Home Work

--- app/src/main/java/com/example/sheikhhasina/MainActivity.java
+++ app/src/main/java/com/example/sheikhhasina/MainActivity.java
@@ -1,19 +1,45 @@
 package com.example.sheikhhasina;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+
+        mToolbar = findViewById(R.id.toolbarId);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
+        mToolbar.setTitleTextColor(Color.WHITE);
+
+
+        drawerLayout = findViewById(R.id.drawerLayId);
+        navigationView = findViewById(R.id.navViewId);
+
+        ActionBarDrawerToggle actionBarDrawerToggle =
+                new ActionBarDrawerToggle(this, drawerLayout, mToolbar, R.string.navigation_open, R.string.navigation_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+        actionBarDrawerToggle.syncState();
+
 
 
 
